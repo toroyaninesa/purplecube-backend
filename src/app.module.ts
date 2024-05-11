b0fbe6f55dc5acc90729a7c56e3b622bcbb04a05
Toroyan <toroyaninesa@gmail.com>
2024-05-11 15:19:01 +0400

    production env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ConfigModule } from '@nestjs/config';
 
-const envFilePath = 'src/common/env/development.env';
+const development = 'src/common/env/development.env';
+const production = 'src/common/env/production.env';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ConfigModule.forRoot({ envFilePath: getEnvironmentConfigFile(), isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     ApiModule,
   ],
 })
 export class AppModule {}
+
+function getEnvironmentConfigFile() {
+  return process.env.NODE_ENV === 'production' ? production : development;
+}
